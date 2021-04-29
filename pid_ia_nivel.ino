5e8b086ba88e3f2cb062c69f994f612452abea95
João Marcos <joaomarcossm2013@hotmail.com>
2021-04-29 08:48:32 -0300

    operation mode manual created, supervisory reading and writting values

--- pid_ia_nivel.ino
+++ pid_ia_nivel.ino
@@ -40,10 +40,26 @@ uint16_t tab_reg[20];
 #define V1 3          // V1 será relé que acionará válvula 1
 #define V2 2          // V2 será relé que acionará válvula 2
 
-// Funções e variaveis p/ sensor ultrassônico
-void trigPuls();      //Função p/ gerar o pulso de trigger
-float pulse;          //Variável p/ armazenar o tempo de pulso echo
-float distance;       //Variável p/ armazenar a distância em cm
+//Variaveis p/ sensor ultrassônico
+float pulse,          //Variável p/ armazenar o tempo de pulso echo
+      distance;       //Variável p/ armazenar a distância em centímetros
+
+      
+// Configurações do PID
+float pv = 0,         //valor do processo (nível em cm)
+      sp = 0,         //valor desejável ou escolhido para o processo (nível em cm)
+      erro = 0,       //erro ou offset do sistema (sp-pv)
+      kp = 0,         //constante kp
+      ki = 0,         //constante ki
+      kd = 0,         //constante kd
+      proportional = 0,  //armazena valor proporcional
+      integral = 0,   //armazena valor integral
+      derivative = 0, //armazena valor derivativo
+      PID = 0,        //armazena resultado PID
+      time_process = 1;  //tempo de 1 segundo
+
+// Variável p/ modo de funcionamento da planta
+int mode = 0;
 
 // Ajustes iniciais
 void setup()
@@ -94,7 +110,135 @@ void setup()
   
 }
 
+// Processo do Arduíno
 void loop()
 {
+  triggerPulse();                //Aciona função do trigger do módulo ultrassônico  
+  pulse = pulseIn(ECHO, HIGH, 200000);   //Medindo o tempo de ECHO em nível alto
+  distance = pulse/58;           //converte resultado da distância em centímetros
+
+  //converte distância em nível no tanque
+  pv = 35-distance;
+
+  //calcula o erro do processo
+  erro = sp-pv;
   
+  //Atribuindo variáveis com tags de comunicação
+    //Velocidades das bombas 0 à 1023
+    /* convertendo para escala de 0 à 100
+          1023 -- 100
+             x -- 1
+             x = 10.23 */
+  float ar_pwm1 = analogRead(PWM1)/10.23;
+  float ar_pwm2 = analogRead(PWM2)/10.23;
+
+  //tab_reg[0] = ar_pwm1;
+  //tab_reg[1] = ar_pwm2;
+  
+  //Medição do nível sensor ultrassônico
+  tab_reg[2] = pv;
+  
+  //Valor desejável para o nível
+  tab_reg[3] = sp;
+  
+  //Constantes PID
+  tab_reg[4] = kp;
+  tab_reg[5] = ki;
+  tab_reg[6] = kd;
+  
+  //Receitas
+  //tab_reg[7] = receita;
+  
+  //Válvulas (shield Relé)
+  tab_reg[8] = digitalRead(V1);
+  tab_reg[9] = digitalRead(V2);
+  
+  //Chaves de nível
+  tab_reg[10] = digitalRead(LSL1);
+  tab_reg[11] = digitalRead(LSL2);
+  tab_reg[12] = digitalRead(LSH1);
+  tab_reg[13] = digitalRead(LSH2);
+
+  //Chaves sentido horário bombas 1 e 2
+  tab_reg[14] = digitalRead(INT1);
+  tab_reg[15] = digitalRead(INT3);
+
+  //Modo de funcionamento da planta
+  tab_reg[16] = mode;
+  
+    
+  //Comunicando com supervisório
+  modbusino_slave.loop(tab_reg, 20);
+
+  /*Atribuindo atualização do supervisório no Arduíno*/
+  
+  mode = tab_reg[16];
+  
+
+  //Modo de funcionamento manual selecionado
+  if(mode == 1)
+  {
+    
+    //Velocidades das bombas 0 à 100
+    /* convertendo para escala de 0 à 255
+          255 -- 100
+            x -- 1
+            x = 2.55 */
+    float aw_pwm1 = tab_reg[0]*2.55;
+    float aw_pwm2 = tab_reg[1]*2.55;
+    
+    analogWrite(PWM1, aw_pwm1);
+    analogWrite(PWM2, aw_pwm2);
+    
+    sp = tab_reg[3];
+    kp = tab_reg[4];
+    ki = tab_reg[5];
+    kd = tab_reg[6];
+    
+    digitalWrite(V1, tab_reg[8]);
+    digitalWrite(V2, tab_reg[9]);
+    
+    digitalWrite(INT1, tab_reg[14]);
+    digitalWrite(INT3, tab_reg[15]);
+    mode = tab_reg[16];    
+    
+  }
+  //Modo Automático (PID) selecionado
+  else if (mode == 2)
+  {
+
+    
+  }
+  //Modo Inteligência Artificial (python) selecionado 
+  else if (mode == 3)
+  {
+
+    
+  }
+  //Nenhum ou muitos modos selecionados, planta desligada, apenas realiza leituras
+  else
+  {
+    
+    analogWrite(PWM1, 0);
+    analogWrite(PWM2, 0);
+    
+    sp = tab_reg[3];
+    kp = tab_reg[4];
+    ki = tab_reg[5];
+    kd = tab_reg[6];
+
+    digitalWrite(V1, 1);
+    digitalWrite(V2, 1);
+    
+    digitalWrite(INT1, 0);
+    digitalWrite(INT3, 0);
+  }
+
+  
+}
+void triggerPulse()
+{
+  digitalWrite(TRIGGER, HIGH);  //Pulso de trigger em nível alto
+  delayMicroseconds(10);        //tempo de 10 micro segundos
+  digitalWrite(TRIGGER, LOW);   //Pulso de trigger em nível baixo
 }
